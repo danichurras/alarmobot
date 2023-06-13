56f830a649c6a71a50ff9334cb2bf3b4e017e113
Daniel Chaves <danielchaves@uepg.br>
2023-06-13 10:17:53 -0300

    api e eventos setados para subscribe e publish

--- app/Http/Controllers/MqttApiController.php
+++ app/Http/Controllers/MqttApiController.php
@@ -2,19 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\MqttMessageReceived;
 use Illuminate\Http\Request;
 use PhpMqtt\Client\Facades\MQTT;
 
 class MqttApiController extends Controller
 {
-    public function publish(Request $request) {
+    /**
+     * Api para publicar uma mensagem em algum topico.
+     */
+    public function publish(Request $request)
+    {
         $validatedRequest = $request->validate(['topic' => 'required', 'message' => 'required']);
-        
+
         $topic = $validatedRequest['topic'];
         $message = $validatedRequest['message'];
 
         $mqtt = MQTT::connection();
-        $mqtt->publish($topic, $message, 2, true); // QoS 2 Retain the message
+        $mqtt->publish($topic, $message, 2);
+        $mqtt->loop(true, true);
+
+        return response()->json();
+    }
+
+    /**
+     * Api para se inscrever em algum topico.
+     */
+    public function subscribe(Request $request)
+    {
+        $validatedRequest = $request->validate([
+            'topic' => 'required',
+            'message' => 'required'
+        ]);
+
+        $topic = $validatedRequest['topic'];
+        $message = $validatedRequest['message'];
+
+        $mqtt = MQTT::connection();
+        $mqtt->subscribe($topic, function (string $topic, string $message) {
+            MqttMessageReceived::dispatch($topic, $message);
+        }, 2);
         $mqtt->loop(true, true);
 
         return response()->json();
---
+++ app/Listeners/SaveMqttMessageDatabase.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
+
+class SaveMqttMessageDatabase
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(object $event): void
+    {
+        Log::info("Subscribe no topic $event->topic retornou uma mensagem $event->message.");
+    }
+}
